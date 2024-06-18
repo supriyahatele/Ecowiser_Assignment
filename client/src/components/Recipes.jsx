f5ca1ec3b58edbe8095489e348a0e49402506de9
supriyahatele <niyahatele70@gmail.com>
2024-06-18 23:58:26 +0530

    hero page updated

--- client/src/components/Recipes.jsx
+++ client/src/components/Recipes.jsx
@@ -1,42 +1,57 @@
-import { BASE_URL } from '../utils/vars';
-import React, { useEffect, useState } from 'react';
-import { Box, Grid, Spinner, Input, InputGroup, Heading, InputLeftElement, Text, Button, Image, Center } from '@chakra-ui/react';
-import { SearchIcon } from '@chakra-ui/icons';
-import axios from 'axios';
-import RecipeCard from './RecipeCard';
+import { BASE_URL } from "../utils/vars";
+import { useEffect, useState } from "react";
+import {
+  Box,
+  Grid,
+  Spinner,
+  Input,
+  InputGroup,
+  Heading,
+  InputRightElement,
+  Text,
+  Button,
+  Image,
+  Stack,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+import axios from "axios";
+import RecipeCard from "./RecipeCard";
 
 const Recipe = () => {
   const [recipes, setRecipes] = useState([]);
   const [filteredRecipes, setFilteredRecipes] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [search, setSearchQuery] = useState('');
+  const [search, setSearchQuery] = useState("");
   const [error, setError] = useState(null);
   const [page, setPage] = useState(1);
 
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
-        const response = await axios.get(`${BASE_URL}/recipe/getAllRecipes?page=${page}`);
+        const response = await axios.get( 
+         ` ${BASE_URL}/recipe/getAllRecipes?page=${page}`
+        );
         setRecipes(response.data.recipes);
         setFilteredRecipes(response.data.recipes);
       } catch (error) {
-        setError('Error fetching recipes. Please try again later.');
-        console.error('Error fetching recipes:', error);
+        setError("Error fetching recipes. Please try again later.");
+        console.error("Error fetching recipes:", error);
       } finally {
         setLoading(false);
       }
     };
 
     fetchRecipes();
-  }, [page]); 
+  }, [page]);
 
   useEffect(() => {
     setFilteredRecipes(
-      recipes.filter((recipe) =>
-        recipe.title.toLowerCase().includes(search.toLowerCase()) ||
-        recipe.ingredients.some((ingredient) =>
-          ingredient.toLowerCase().includes(search.toLowerCase())
-        )
+      recipes.filter(
+        (recipe) =>
+          recipe.title.toLowerCase().includes(search.toLowerCase()) ||
+          recipe.ingredients.some((ingredient) =>
+            ingredient.toLowerCase().includes(search.toLowerCase())
+          )
       )
     );
   }, [search, recipes]);
@@ -51,7 +66,12 @@ const Recipe = () => {
 
   if (loading) {
     return (
-      <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
         <Spinner size="xl" />
       </Box>
     );
@@ -59,55 +79,88 @@ const Recipe = () => {
 
   if (error) {
     return (
-      <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
-        <Text fontSize="xl" color="red.500">{error}</Text>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <Text fontSize="xl" color="red.500">
+          {error}
+        </Text>
       </Box>
     );
   }
 
   return (
-    <Box p="6">
-      <Center mb="3" height="300px"  width="90%" margin="auto">
-        <Box position="relative"  >
+    <Box p="2">
+      <Stack height="450px" width="100%">
+        <Box position="relative" height="100%" width="98%" margin={"auto"} p="10px">
           <Image
             src="https://media.istockphoto.com/id/1423092276/photo/business-lunch-in-cafe-tasty-food-on-table-pizza-with-greenery-mozzarella-cheese-bacon-fried.jpg?b=1&s=612x612&w=0&k=20&c=i-RDfPsjYzsYxF61IhTLAVtpp0eYWsAODgNdfPSByoc="
             alt="Delicious food"
-            borderRadius="md"
+            boxSize="100%"
             objectFit="cover"
-            width="100vh"
-            height="300px"
-            opacity='0.7'
+            opacity="0.8"
+            borderRadius="20px"
           />
-          <Box position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)" color="white"  p="4" borderRadius="md">
-            <Text fontSize="3xl" fontWeight="medium">"Good food is the foundation of happiness."</Text>
+          <Box
+            position="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+            color="white"
+            p="1px"
+            borderRadius="md"
+            textAlign={"center"}
+            w="90%"
+          >
+            <Text
+              fontSize="4xl"
+              fontWeight={"700"}
+              textShadow={"4px  4px 4px white"}
+              textDecoration={"underline"}
+              color={"rgb(175, 20, 20)"}
+            >
+              Good food is the foundation of happiness.
+            </Text>
+
+            <InputGroup width="50%" margin={"auto"} mt="50px">
+              <InputRightElement pointerEvents="none">
+                <SearchIcon
+                  color="white"
+                  mt="20px"
+                  mr="20px"
+                  height={"20px"}
+                  w="20px"
+                />
+              </InputRightElement>
+              <Input
+              bg='rgba(0, 0, 0, 0.5)'
+                border={"5px solid white"}
+                borderRadius={"25px"}
+                h="50px"
+                fontWeight={"500"}
+                fontSize="2xl"
+                type="text"
+                color={"white"}
+                placeholder="Search recipes..."
+                _placeholder={{
+                  color: "white",
+                  fontWeight: "bold",
+                  fontSize: "sm",
+                }}
+                value={search}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+            </InputGroup>
           </Box>
         </Box>
-      </Center>
-      
-      <InputGroup  margin={"auto"} mb="6"  mt="6"
-       width={{
-        base: "60%",
-        sm: "60%",
-        md: "40%",
-        lg: "40%",
-        xl: "30%",
-        "2xl": "30%",
-      }}
-       >
-        <InputLeftElement pointerEvents="none">
-          <SearchIcon color="gray.300" />
-        </InputLeftElement>
-        <Input
-          type="text"
-          placeholder="Search recipes..."
-          value={search}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-      </InputGroup>
+      </Stack>
 
-      <Grid 
-        // templateColumns="repeat(auto-fill, minmax(300px, 1fr))" 
-        gap="20px" 
+
+      <Grid
+        gap="20px"
         p="20px"
         templateColumns={{
           base: "repeat(1,1fr)",
@@ -115,7 +168,7 @@ const Recipe = () => {
           md: "repeat(2,1fr)",
           lg: "repeat(3,1fr)",
           xl: "repeat(3,1fr)",
-          "2xl": "repeat(4,1fr)",
+          "2xl": "repeat(5,1fr)",
         }}
       >
         {filteredRecipes.map((recipe) => (
@@ -123,20 +176,32 @@ const Recipe = () => {
         ))}
       </Grid>
 
-      <Box 
-        width="100%" 
-        mt="30px" 
+      <Box
+        width="100%"
+        mt="30px"
         display="flex"
         justifyContent="center"
         alignItems="center"
         flexDirection="row"
         p="10px"
       >
-        <Button mr={5} w="100px" h="50px" onClick={handlePrevPage} disabled={page === 1}>
+        <Button
+          mr={5}
+          w="100px"
+          h="50px"
+          onClick={handlePrevPage}
+          disabled={page === 1}
+        >
           Prev
         </Button>
         <Heading fontSize="lg">{page}</Heading>
-        <Button ml={5} w="100px" h="50px" onClick={handleNextPage} disabled={filteredRecipes.length < 1}>
+        <Button
+          ml={5}
+          w="100px"
+          h="50px"
+          onClick={handleNextPage}
+          disabled={filteredRecipes.length < 1}
+        >
           Next
         </Button>
       </Box>
